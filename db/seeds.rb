1fb525f6a30c61db8bcb366b74a6e828234e0732
Maria Castro <mariacantocastro@gmail.com>
2018-05-24 23:51:27 +0100

    app finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create name: 'Boa Bao', address: 'Trindade', phone_number: '916784237'
+Restaurant.create name: 'Sea me', address: 'Chiado', phone_number: '916435637'
+Restaurant.create name: 'The B temple', address: 'Chiado', phone_number: '916781237'
+Restaurant.create name: 'Lisbon Winery', address: 'Bairro Alto', phone_number: '916904237'
+
